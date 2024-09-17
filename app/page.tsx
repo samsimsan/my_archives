a7be18c9d5ff49f0766e536db7ee8c98ff46ba86
Sam Simsan <samsimsan@gmail.com>
2024-09-17 17:01:39 +0530

    updated main page

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,22 @@ export default function Home() {
   return (
     <div className="bg-[#E9C46A] text-foreground h-[100vh] pt-32 pl-32 text-[#023047]">
       <main className="">
-        <h2 className="text-3xl font-semibold">
+        <h2 className="text-3xl ">
           Hi! I'm Sam and this is
         </h2>
         <h1 className="mainheading text-9xl font-bold mb-4">
           My Archives
         </h1>
-        <p className="font-semibold">A place to store my ideas and rough concepts</p>
-        <p className="font-semibold">Explore below!</p>
+        <p className="">Find my latest solutions from frontend mentor hosted here!</p>
+        <p className="">Explore below!</p>
         <br />
-        <div className=" grid gap-4 justify-start">
-          <Button asChild>
-            <Link
-              className="font-semibold tracking-wide"
-              href="/Mortgage_repayment_calculator">
-              Mortgage repayment calculator
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link
-              className="font-semibold tracking-wide"
-              href="/tester">
-              Tester
-            </Link>
-          </Button>
+        <div className="grid gap-4 justify-start">
+          <Link
+            className="hover:text-sky-800 transition ease-in-out duration-150 underline hover:underline-offset-4 underline-offset-3 uder tracking-wide text-xl"
+            href="/Mortgage_repayment_calculator">
+            Mortgage repayment calculator
+          </Link>
+          
         </div>
       </main>
     </div>
