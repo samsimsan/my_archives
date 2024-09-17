5f2ed18a7dd6ade36d05e81853faed4c606af56a
Sam Simsan <samsimsan@gmail.com>
2024-09-17 16:43:59 +0530

    added footer

--- app/Mortgage_repayment_calculator/layout.tsx
+++ app/Mortgage_repayment_calculator/layout.tsx
@@ -15,6 +15,9 @@ const layout = ({
         <html lang="en">
             <body className={`${plus_Jakarta_Sans.variable} font-Jakarta`}>
                 {children}
+                <footer className="absolute bottom-5 text-sm font-semibold opacity-50 underline underline-offset-2">
+                    Made by Sam P S · 2024 · <a className="text-indigo-700" href="https://www.frontendmentor.io/profile/samsimsan">FrontEnd Mentor challenge</a>
+                </footer>
             </body>
         </html>
     )
